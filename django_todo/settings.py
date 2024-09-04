0a3141d812a2dc6a77082e3530b71ca59849f352
Emily Page <epageone81@gmail.com>
2024-09-04 16:51:10 +0000

    fixed allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '2erm2g25fi&()6+d)rp0(1gw!vp^v2aoxpmfebc^1_2gbjc-@#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [epageone-django-todo-app-04e2960c7281.herokuapp.com]
+ALLOWED_HOSTS = ['epageone-django-todo-app-04e2960c7281.herokuapp.com']
 
 
 # Application definition
